aea17cfd58017c363a1e4261c448f9decf69dcaf
Anthony Goldhammer <Anthonygoldhammer03@gmail.com>
2024-06-05 19:09:52 -0400

    added about me and expereince

--- src/AboutMeContent.tsx
+++ src/AboutMeContent.tsx
@@ -1,5 +1,26 @@
-function AboutMeContent(){
-    return <div>About Me</div>
+import React from "react";
+
+function AboutMeContent() {
+  return (
+    <div className="home_row">
+      <div className="home_column">
+        <h1 className="aboutme_header1">Hey, Im Anthony Goldhammer</h1>
+        <h2 className="home_text1">
+          Hello, I’m Anthony, a Junior Computer Science student at the
+          University of South Carolina. I'm a Software Engineer passionate about
+          making innovative applications and websites. My journey into
+          programming began at 17 when I discovered the thrill of bringing ideas
+          to life through code. I'm particularly drawn to projects that
+          challenge me to explore new technologies and problem-solving
+          approaches. Im excited to delve deeper into the realms of Full Stack
+          Website Development and continue honing my skills.
+        </h2>
+      </div>
+      <div className="home_column2">
+        <div className="about_me_image"></div>
+      </div>
+    </div>
+  );
 }
 
 export default AboutMeContent;
--- src/ExperienceContent.tsx
+++ src/ExperienceContent.tsx
@@ -6,15 +6,29 @@ function ExperienceContent(){
    
 
     return (
-    <div className="home_row">
-        <div className="home_column">
-            <h1 className="home_header1">Experience</h1>
+    <div className="experience_row">
+        <div className="experience_column1">
+            <h1 className="expereince_header1">Experience</h1>
             <h2 className="experience_title1">Spencers/Spirit Halloween</h2>
             <h2 className="experience_title1">Warehouse 2020-2023</h2>
-            <h3 className="experience_Spencers1"></h3>
-                 
-            
+            <h3 className="experience_spencers1">Loading Bay</h3>
+            <h3 className="experience_spencers1">Picker</h3>
+            <h3 className="experience_spencers1">Back Stock</h3>
         </div>
+        <div className="experience_column2">
+            
+            <h2 className="experience_title2">SCLPT</h2>
+            <h3 className="experience_sclpt1">Full Stack Development</h3>
+            <h3 className="experience_sclpt2">React</h3>
+            <h3 className="experience_sclpt3">TypeScript</h3>
+            <h3 className="experience_sclpt4">Postgresql</h3>
+        </div>
+        <div className="experience_column3">
+            
+            <h2 className="experience_title3">UofSC Projects 2023-2024</h2>
+            <h3 className="experience_uofsc1">Java Website w/ JavaFX</h3>
+        </div>
+
     </div>
     );
 }
