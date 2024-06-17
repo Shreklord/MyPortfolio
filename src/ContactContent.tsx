74e16e7b92361c464b6fe91abf17e0094bb89e3c
Anthony Goldhammer <Anthonygoldhammer03@gmail.com>
2024-06-17 15:14:49 -0400

    Edits on Experience page

--- src/ContactContent.tsx
+++ src/ContactContent.tsx
@@ -9,8 +9,8 @@ function ContactContent() {
         <h1 className="contact_header">Contact</h1>
       </div>
       <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap", gap:"30px", paddingTop:"50px", paddingLeft:"350px", position:"relative" }}>
-        <SiGmail size={400} color="white" />
-        <CiCloudOn size={400} color="white" />
+        <SiGmail size={390} color="white" />
+        <CiCloudOn size={470} color="white" />
         <CiInstagram size={400} color="white" />
         <CiLinkedin size={400} color="white" />
       </div>
--- src/ExperienceContent.tsx
+++ src/ExperienceContent.tsx
@@ -6,11 +6,15 @@ function ExperienceContent(){
    
 
     return (
+    <div>
+    <div className="experience_row_header">
+        <div>
+            <h1 className="expereince_header1">Experience</h1>
+        </div>
+    </div>
     <div className="experience_row">
         <div className="experience_column1">
-            <h1 className="expereince_header1">Experience</h1>
-            <h2 className="experience_title1">Spencers/Spirit Halloween</h2>
-            <h2 className="experience_title1">Warehouse 2020-2023</h2>
+            <h2 className="experience_title1">Spencers/Spirit Halloween Warehouse 2020-2023</h2>
             <h3 className="experience_spencers1">Loading Bay</h3>
             <h3 className="experience_spencers1">Picker</h3>
             <h3 className="experience_spencers1">Back Stock</h3>
@@ -29,6 +33,7 @@ function ExperienceContent(){
             <h3 className="experience_uofsc1">Java Website w/ JavaFX</h3>
         </div>
 
+    </div>
     </div>
     );
 }
