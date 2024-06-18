da5f44b74de922dfcc64900ea548c8212e7020c2
kylemcshea <kmcshea@stevens.edu>
2024-06-17 21:32:32 -0400

    boilerplate: navigation bar for mobile initialization

--- src/NavigationBar.tsx
+++ src/NavigationBar.tsx
@@ -1,4 +1,5 @@
 // import React, { useState } from "react";
+import "./navigation_bar.css";
 
 type NavigationBarProps = {
   activeTab: string;
@@ -21,21 +22,24 @@ function NavigationBar({ activeTab, setActiveTab }: NavigationBarProps) {
 
   return (
     <nav>
-      <div className="nav_row">
-        <div className="name_column">
-          <h2 className="name_header">Anthony Goldhammer</h2>
-          <div className="green_bar" />
-        </div>
-        <div className="tabs_row">
-          {tabs.map((tab) => (
-            <div
-              key={tab}
-              className={activeTab === tab ? "nav_tab_active" : "nav_tab"}
-              onClick={() => handleTabClick(tab)}
-            >
-              {tab}
-            </div>
-          ))}
+      <div className="nav_mobile_container">Hamburger</div>
+      <div className="nav_desktop_container">
+        <div className="nav_row">
+          <div className="name_column">
+            <h2 className="name_header">Anthony Goldhammer</h2>
+            <div className="green_bar" />
+          </div>
+          <div className="tabs_row">
+            {tabs.map((tab) => (
+              <div
+                key={tab}
+                className={activeTab === tab ? "nav_tab_active" : "nav_tab"}
+                onClick={() => handleTabClick(tab)}
+              >
+                {tab}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </nav>
