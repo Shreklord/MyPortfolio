c2886b6d4510cd1f816bcfe2e0fd3e02c9b5ddd0
Shreklord <goldhama@email.sc.edu>
2024-10-03 14:16:57 -0400

    Responsiveness Update

--- src/ExperienceContent.tsx
+++ src/ExperienceContent.tsx
@@ -22,23 +22,23 @@ function ExperienceContent(){
     </div>
     <div className="experience_row">
         <div className="experience_column1">
-            <h2 className="experience_title1">Spencers/Spirit Halloween Warehouse 2020-2023</h2>
-            <h3 className="experience_spencers1"><FaTruckLoading color="#67d45e"/>   Loading Bay</h3>
-            <h3 className="experience_spencers1"><FaHandPaper color="#67d45e"/>   Picker</h3>
-            <h3 className="experience_spencers1"><FaBoxes color="#67d45e"/>   Back Stock</h3>
-        </div>
-        <div className="experience_column2">
-            
             <h2 className="experience_title2">SCLPT (Summer 2024, Intern)</h2>
             <h3 className="experience_sclpt1"><BsStack color="#67d45e"/>   Full Stack Development</h3>
             <h3 className="experience_sclpt2"><FaReact color="#67d45e"/>   React</h3>
             <h3 className="experience_sclpt3"><SiTypescript color="#67d45e"/>   TypeScript</h3>
             <h3 className="experience_sclpt4"><BiLogoPostgresql color="#67d45e"/>   Postgresql</h3>
         </div>
-        <div className="experience_column3">
+        <div className="experience_column2">
             
-            <h2 className="experience_title3">UofSC Projects 2023-2024</h2>
+            <h2 className="experience_title3">UofSC Projects (2023-2024)</h2>
             <h3 className="experience_uofsc1"><FaJava color="#67d45e"/>  Java Website w/ JavaFX</h3>
+        </div>
+        <div className="experience_column3">
+            <h2 className="experience_title1">Spencers Warehouse (2020-2024)</h2>
+            <h3 className="experience_spencers1"><FaTruckLoading color="#67d45e"/>   Loading Bay</h3>
+            <h3 className="experience_spencers1"><FaHandPaper color="#67d45e"/>   Picker</h3>
+            <h3 className="experience_spencers1"><FaBoxes color="#67d45e"/>   Back Stock</h3>
+            
         </div>
 
     </div>
